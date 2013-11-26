7136425d478672e2aa6843bc2b45b664627e3e9c
dospatos <sean@concernsoftware.com>
2013-11-26 15:43:07 -0500

    Added framework

--- index.php
+++ index.php
@@ -1,68 +1,36 @@
 <?php
-/*
- * Created by JetBrains PhpStorm.
- * User: skraft
- * Date: 9/6/13
- * Time: 3:12 PM
- * Create by Sean Kraft for CSCI-E15 Fall 2013
- */
+
+# The DOC_ROOT and APP_PATH constant have to happen in the actual app
+
+	# Document root, ex: /path/to/home/app.com/../ (uses ./ on CLI)
+	define('DOC_ROOT', empty($_SERVER['DOCUMENT_ROOT']) ? './' : realpath($_SERVER['DOCUMENT_ROOT']).'/../');
+	  
+	# App path, ex: /path/to/home/app.com/
+	define('APP_PATH', realpath(dirname(__FILE__)).'/');
+         
+# Environment
+	require_once DOC_ROOT.'environment.php'; 
+   
+# Where is core located?
+	define('CORE_PATH',  $_SERVER['DOCUMENT_ROOT']."/../core/");
+	   
+# Load app configs
+	require APP_PATH."/config/config.php";
+	require APP_PATH."/config/feature_flags.php";
+	  
+# Bootstrap
+	require CORE_PATH."bootstrap.php";
+
+# Routing
+# You can use regexp, for example '/users/([A-Za-z0-9-\s\@]+)' => '/members/$0',
+    Router::$routes = array(
+    	'/' => '/index',     # default controller when "/" is requested
+    );
+    
+# Match requested uri to any routes and instantiate controller
+    Router::init();
+    
+# Display environment details
+	require CORE_PATH."environment-details.php";
+	
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Sean Kraft's CSCI E-15 Web Page</title>
-<link type="text/css" rel="stylesheet" href="basic-minimal.css">
-<script src="js/jquery-1.10.2.js"> </script>
-<script type="text/javascript" src="js/jquery-1.10.2.js"></script>
-<script type="text/javascript" src="js/jquery.ui.core.js"></script>
-<script type="text/javascript" src="js/jquery.ui.widget.js"></script>
-<script type="text/javascript" src="js/jquery.ui.timer.js"></script>
-
-
-</head>
-
-<body>
-
-<article style="width:700px">
-    <header>
-        <h2>Sean Kraft CSCI E-15</h2>
-    </header>
-
-
-    <h2>Javascript Timer</h2>
-    <p>Behold the jQuery Timer plugin. Turns an HTML canvas into a handsome countdown clock!</p>
-    <section>
-        <canvas id="TimerDisplay2" width="150" height="200"></canvas>
-        <canvas id="TimerDisplay3" width="150" height="200"></canvas>
-        <div id="TimerDisplay4" style="width:150px;height:200px;background-color:red;color: black;font-family: Arial;font-size: 20px;"/>
-
-    </section>
-
-    <footer>
-        <p>Posted by: Sean Kraft</p>
-        <p><time datetime="2013-09-15"></time></p>
-    </footer>
-
-</article>
-
-</body>
-<script language="javascript">
-
-    $( document ).ready(function() {
-        $("#TimerDisplay2").timer({ added: function(e, ui){}
-            , minutesAllowed:30
-            ,timeup: function(e,ui){alert('timeup');}});
-
-        $("#TimerDisplay3").timer({ added: function(e, ui){}
-            , minutesAllowed:1
-            ,timeup: function(e,ui){alert('timeup 3');}});
-
-        $("#TimerDisplay4").timer({ added: function(e, ui){}
-            , minutesAllowed:.5
-            ,timeup: function(e,ui){alert('timeup 4');}});
-        //$("#iceland_dog").timer({ added: function(e, ui){ });
-    });
-
-</script>
-</html>
