20d6e89a31bc0d88d7dc5da84def79a46824236f
Sean Kraft <sean@concernsoftware.com>
2013-11-27 11:29:37 -0500

    updates

--- controllers/c_index.php
+++ controllers/c_index.php
@@ -12,26 +12,14 @@ class index_controller extends base_controller {
 	/*-------------------------------------------------------------------------------------------------
 	Accessed via http://localhost/index/index/
 	-------------------------------------------------------------------------------------------------*/
-	public function index() {	
+	public function increment($counter_id = null) {
 		
-		# Any method that loads a view will commonly start with this
-		# First, set the content of the template with a view file
-			$this->template->content = View::instance('v_index_index');
-			
-		# Now set the <title> tag
-			$this->template->title = "Hello World";
-	
-		# CSS/JS includes
-			/*
-			$client_files_head = Array("");
-	    	$this->template->client_files_head = Utils::load_client_files($client_files);
-	    	
-	    	$client_files_body = Array("");
-	    	$this->template->client_files_body = Utils::load_client_files($client_files_body);   
-	    	*/
-	      					     		
-		# Render the view
-			echo $this->template;
+		    //if there is no counter ID we'll create a new one
+
+            //otherwise increment the counter by one
+
+
+			echo "";//json object here
 
 	} # End of method
 	
